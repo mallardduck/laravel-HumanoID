0a4da48a5433d83bea66dcf80219d53daa084443
Dan <self@danpock.me>
2022-04-24 21:11:47 -0400

    kill some mutants

--- tests/FacadesTest.php
+++ tests/FacadesTest.php
@@ -1,16 +1,26 @@
 <?php
 
-use MallardDuck\LaravelHumanoID\Facades\HumanoID;
-use MallardDuck\LaravelHumanoID\Facades\HumanoIDManager;
+use MallardDuck\LaravelHumanoID\Facades\HumanoID as HumanoIDFacade;
+use MallardDuck\LaravelHumanoID\Facades\HumanoIDManager as HumanoIDManagerFacade;
+use RobThree\HumanoID\HumanoID;
 
 it('can verify HumanoIDManager facade exists', function () {
-    expect(class_exists(HumanoIDManager::class))->toBeTrue();
-    expect(HumanoIDManager::class)->callStatic('getFacadeAccessor')->toBeString()->toBe('laravel-humanoid');
+    expect(class_exists(HumanoIDManagerFacade::class))->toBeTrue();
+    expect(HumanoIDManagerFacade::class)->callStatic('getFacadeAccessor')->toBeString()->toBe('laravel-humanoid');
+});
+
+it('can verify HumanoIDManager facade aliases exists', function () {
+    /**
+     * @var \Illuminate\Foundation\Application $app
+     */
+    $app = $this->app;
+    expect($app->has('laravel-humanoid'))->toBeTrue();
+    expect($app->has(\MallardDuck\LaravelHumanoID\HumanoIDManager::class))->toBeTrue();
 });
 
 it('can use the HumanoIDManager facade', function () {
-    expect(HumanoIDManager::hasWordSetsFolder())->toBeTrue();
-    $generator = HumanoIDManager::getDefaultGenerator();
+    expect(HumanoIDManagerFacade::hasWordSetsFolder())->toBeTrue();
+    $generator = HumanoIDManagerFacade::getDefaultGenerator();
     expect($generator)
         ->toBeInstanceOf(\RobThree\HumanoID\HumanoID::class);
     expect($generator->create(42))
@@ -22,15 +32,24 @@ it('can use the HumanoIDManager facade', function () {
 });
 
 it('can verify HumanoID facade exists', function () {
-    expect(class_exists(HumanoID::class))->toBeTrue();
-    expect(HumanoID::class)->callStatic('getFacadeAccessor')->toBeString()->toBe('humanoid');
+    expect(class_exists(HumanoIDFacade::class))->toBeTrue();
+    expect(HumanoIDFacade::class)->callStatic('getFacadeAccessor')->toBeString()->toBe('humanoid');
+});
+
+it('can verify HumanoID facade aliases exists', function () {
+    /**
+     * @var \Illuminate\Foundation\Application $app
+     */
+    $app = $this->app;
+    expect($app->has('humanoid'))->toBeTrue();
+    expect($app->has(HumanoID::class))->toBeTrue();
 });
 
 it('can use the HumanoID facade', function () {
-    expect(HumanoID::create(42))
+    expect(HumanoIDFacade::create(42))
         ->toBeString()
         ->toBe('Haumea-Pinwheel');
-    expect(HumanoID::parse('Aperture-Blue-Remnant-Hypergiant-Venus-Cigar'))
+    expect(HumanoIDFacade::parse('Aperture-Blue-Remnant-Hypergiant-Venus-Cigar'))
         ->toBeInt()
         ->toBe(42069);
 });
